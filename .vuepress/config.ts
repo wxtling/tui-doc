e10954bcfa54bf0f4f647040e8858e33d1e2d65e
xttling <et-al@qq.com>
2022-03-07 21:52:33 +0800

    1.0.0-alpha

---
+++ .vuepress/config.ts
@@ -0,0 +1,64 @@
+import type { DefaultThemeOptions } from 'vuepress';
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: 'TUI',
+  description: '一个开箱即用的 uni-app 框架',
+  themeConfig: {
+    editLink: false,
+    repo: 'https://gitee.com/etal/tui-doc',
+
+    navbar: [
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+      {
+        text: '组件',
+        link: '/components/',
+      },
+      {
+        text: 'API 参考',
+        link: '/api/',
+      },
+      {
+        text: '关于',
+        link: '/about/',
+      },
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: '指南',
+          children: ['/guide/index.md', '/guide/start.md'],
+        },
+      ],
+    },
+
+    // page meta
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdatedText: '上次更新',
+    contributorsText: '贡献者',
+
+    // custom containers
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+
+    // 404 page
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
+
+    // a11y
+    openInNewWindow: '在新窗口打开',
+    toggleDarkMode: '切换夜间模式',
+    toggleSidebar: '切换侧边栏',
+  },
+});
---
+++ guide/index.md
@@ -0,0 +1,9 @@
+# 介绍
+
+## 简介
+[Tui](https://github.com/wxtling/tui) 是一个基于 [uni-app](https://uniapp.dcloud.io/)、[Vue3](https://v3.cn.vuejs.org/)、[Vite](https://cn.vitejs.dev/)、 [TypeScript](https://www.typescriptlang.org/zh/) 的移动端解决方案，目标是为开发中型项目提供开箱即用的解决方案。包括封装组件、utils、hooks、路由等功能。项目会使用前端较新的技术栈，可以作为项目的启动模版，以帮助你快速搭建移动端产品原型。也可以作为一个示例，用于学习 `vue3`、`vite`、`ts` 等主流技术。该项目会持续跟进最新技术，并将其应用在项目中。
+
+## 文档
+文档地址为 [Tui-doc](https://github.com/wxtling/tui-doc)，采用 vuepress 开发。
+
+正在努力编写中...
